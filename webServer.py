033223814598318336f865ded95fe17adb0eb15d
concept-inversion <santosh.pandey2222@gmail.com>
2018-04-26 07:53:19 +0545

    Update,Delete and create form added

--- webServer.py
+++ webServer.py
@@ -19,19 +19,22 @@ class webServer(BaseHTTPRequestHandler):
     start= '<html><body>'
     table = '''<table style="width:100%">
             <tr>
+              <th>Id</th>
               <th>Name</th>
-              <th>Address</th> 
+              <th>Email</th> 
               <th>Bio</th>
-              <th>Age</th>
               <th>Gender</th>
               <th>Link</th>
               <th>Address</th>
               <th>Longitude</th>
               <th>Latitude</th>
               <th>Image</th>
-              <th>Dob</th>
               <th>Phone</th>
+              <th>Dob</th>
+              
             </tr> ''' 
+    
+    
     
     def setHeaders(self):
         self.send_response(200)
@@ -59,6 +62,7 @@ class webServer(BaseHTTPRequestHandler):
                           <td>{each[9]}</td>
                           <td>{each[10]}</td>
                           <td>{each[11]}</td>
+                        
                         </tr>
                     ''')
                 self.output += '</table></body></html>'
@@ -66,45 +70,124 @@ class webServer(BaseHTTPRequestHandler):
                 self.wfile.write(self.output.encode())
                 return
 
-            if self.path.endswith("/Update"):
-                output = ''
-                output += '<html><body>Update</body></html>'
-                self.setHeaders()
-                self.wfile.write(output.encode())
-                return
-
-            if self.path.endswith("/Create"):
-                output = ''
-                output += '<html><body>Create</body></html>'
-                self.setHeaders()
-                self.wfile.write(output.encode())
-                return
-
-            if path[1]==("View"):
+            if path[1]==("Update"):
+                
                 Id = path[2]
-                print(Id)
                 data = self.db.Read(f"Id = {Id}")
-                output = start + table
-                for each in data:
-                    output +=   (f'''
-                        <tr>
-                          <td>{each[0]}</td>
-                          <td>{each[1]}</td>
-                          <td>{each[2]}</td>
-                          <td>{each[3]}</td>
-                          <td>{each[4]}</td>
-                          <td>{each[5]}</td>
-                          <td>{each[6]}</td>
-                          <td>{each[7]}</td>
-                          <td>{each[8]}</td>
-                          <td>{each[9]}</td>
-                          <td>{each[10]}</td>
-                          <td>{each[11]}</td>
-                        </tr>
-                    ''')
-                output += '</table></body></html>'
+                print(data)
+                self.output += self.start 
+                self.output =   '''
+                                <form method = 'POST' enctype= 'multipart/form-data' action ='update'
+                                '''
+                form = f'''
+                     <br>Name:<br>
+                     <input type="text" name="Name" value="{data[0][1]}">
+                     <br>Email:<br>
+                     <input type="text" name="Email" value="">
+                     <br>Bio:<br>
+                     <input type="text" name="Bio" value="">
+                     <br>Gender:<br>
+                     <input type="text" name="Gender" value="">
+                     <br>Link:<br>
+                     <input type="text" name="Link" value="">
+                     <br>Address:<br>
+                     <input type="text" name="Address" value="">
+                     <br>Longitude:<br>
+                     <input type="text" name="Longitude" value="">
+                     <br>Latitude:<br>
+                     <input type="text" name="Latitude" value="">
+                     <br>Image:<br>
+                     <input type="text" name="Image" value="">
+                     <br>Phone:<br>
+                     <input type="text" name="Phone" value="">
+                     <br>Dob:<br>
+                     <input type="text" name="Dob" value="">
+                     <br><br>
+                     <input type = "submit" value="submit">
+                     </form>
+                    '''
+                if data: 
+                
+                    self.output += form
+                    self.output += '</body></html>'
                 self.setHeaders()
-                self.wfile.write(output.encode())
+                self.wfile.write(self.output.encode())
+                return
+
+            if path[1] == ("Create"):
+                
+                
+                self.output =   '''
+                                <form method = 'POST' enctype= 'multipart/form-data' action ='create'
+                                '''
+                form = '''
+                     
+                     <br>Name:<br>
+                     <input type="text" name="Name" value="">
+                     <br>Email:<br>
+                     <input type="text" name="Email" value="">
+                     <br>Bio:<br>
+                     <input type="text" name="Bio" value="">
+                     <br>Gender:<br>
+                     <input type="text" name="Gender" value="">
+                     <br>Link:<br>
+                     <input type="text" name="Link" value="">
+                     <br>Address:<br>
+                     <input type="text" name="Address" value="">
+                     <br>Longitude:<br>
+                     <input type="text" name="Longitude" value="">
+                     <br>Latitude:<br>
+                     <input type="text" name="Latitude" value="">
+                     <br>Image:<br>
+                     <input type="text" name="Image" value="">
+                     <br>Phone:<br>
+                     <input type="text" name="Phone" value="">
+                     <br>Dob:<br>
+                     <input type="text" name="Dob" value="">
+                     <br><br>
+                     <input type = "submit" value="submit">
+                     </form>
+                    '''
+                self.output += form
+                self.output += '<html><body>Create</body></html>'
+                self.setHeaders()
+                self.wfile.write(self.output.encode())
+                return
+
+            if path[1] == ("Delete"):
+                Id = path[2]
+                data=self.db.Delete(f"Id={Id}")
+                self.output += '<html><body>Deleted</body></html>'
+                self.setHeaders()
+                self.wfile.write(self.output.encode())
+                return
+
+            if path[1] == ("View"):
+                Id = path[2]
+                data = self.db.Read(f"Id = {Id}")
+                self.output = self.start + self.table
+                if data: 
+                    
+                        for each in data:
+                            self.output +=   (f'''
+                            <tr>
+                              <td>{each[0]}</td>
+                              <td>{each[1]}</td>
+                              <td>{each[2]}</td>
+                              <td>{each[3]}</td>
+                              <td>{each[4]}</td>
+                              <td>{each[5]}</td>
+                              <td>{each[6]}</td>
+                              <td>{each[7]}</td>
+                              <td>{each[8]}</td>
+                              <td>{each[9]}</td>
+                              <td>{each[10]}</td>
+                              <td>{each[11]}</td>
+                                </tr>
+                            ''')
+                self.output += '</table></body></html>'
+                self.setHeaders()
+                self.wfile.write(self.output.encode())
                 return 
 
         except IOError:
@@ -113,8 +196,12 @@ class webServer(BaseHTTPRequestHandler):
     def do_POST(self):
         try:
             self.send_response(301)
-            self.end_headers()
-        
+            self.end_headers()    
+            contentType,paramDict = cgi.parse_header(self.headers.getheader('content-type'))
+            if contentType== 'multipart/form-data':
+                fields= cgi.parse_multipart(self.rfile,paramDict)
+                messageContent = fields.get('message')
+
         except:
             IOError
             self.send_error(400)
